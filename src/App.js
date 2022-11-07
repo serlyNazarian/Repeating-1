55e2b1983518420b4d65a66e5319adc54ff68181
serlyNazarian <serlynazarian@gmail.com>
2022-11-07 11:25:18 +0400

    main

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [categories, setCategories] = useState([]);
+  useEffect(() => {
+    fetch("http://asfaltirovanie7-24.ru/api/categories/get")
+      .then((x) => x.json())
+      .then((data) => setCategories(data.categories));
+  }, []);
+  const handlerSubmit = (e) => {
+    e.preventDefault();
+    const data = new FormData(e.currentTarget);
+    let cong = { name: data.get("name") };
+    const fetchConfiguration = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(cong),
+    };
+    fetch(
+      "http://asfaltirovanie7-24.ru/api/categories/store",
+      fetchConfiguration
+    )
+      .then((x) => x.json())
+      .then((x) => setCategories([x.category, ...categories]));
+    // setCategories([...categories]);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handlerSubmit}>
+        <input name={"name"} />
+        <button type={"submit"}>Add</button>
+      </form>
+      <table>
+        <tr>
+          <th>id</th>
+          <th>Name</th>
+        </tr>
+        {categories.map((item) => (
+          <tr>
+            <td>{item.id}</td>
+            <td>{item.name}</td>
+          </tr>
+        ))}
+      </table>
     </div>
   );
 }
